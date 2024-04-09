ed47fcd080428ffd081ab9d010f5988f96c4bebd
kujun88 <kujun@moramcnt.com>
2024-04-09 14:07:40 +0900

    bash_operator dag 업로드

---
+++ dags/dags_bash_operator.py
@@ -0,0 +1,27 @@
+from airflow import DAG
+import datetime
+import pendulum
+from airflow.operators.bash import BashOperator
+
+with DAG(
+    dag_id="dags_bash_operator",
+    schedule="0 0 * * *",
+    start_date=pendulum.datetime(2024, 4, 9, tz="Asia/Seaul"),
+    catchup=False,
+    #dagrun_timeout=datetime.timedelta(minutes=60),
+    #tags=["example", "example2"],
+    #params={"example_key": "example_value"},
+) as dag:
+    # [START howto_operator_bash]
+    bash_t1 = BashOperator(
+        task_id="bash_t1",
+        bash_command="echo whoami",
+    )
+    # [END howto_operator_bash]
+
+    bash_t2 = BashOperator(
+        task_id="bash_t2",
+        bash_command="echo $HOSTNAME",
+    )
+
+    BashOperator >> bash_t2
