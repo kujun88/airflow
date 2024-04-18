33992c81fa45c170ae42104be104935b266a845d
kujun88 <kujun@moramcnt.com>
2024-04-19 08:04:42 +0900

    dags_sla_email_example

--- dags/dags_time_sensor_with_async.py
+++ dags/dags_time_sensor_with_async.py
@@ -4,8 +4,8 @@ from airflow.sensors.date_time import DateTimeSensorAsync
 
 with DAG(
     dag_id="dags_time_sensor_with_async",
-    start_date=pendulum.datetime(2024, 4, 19, 0, 0, 0),
-    end_date=pendulum.datetime(2024, 4, 19, 1, 0, 0),
+    start_date=pendulum.datetime(2024, 4, 18, 0, 0, 0),
+    end_date=pendulum.datetime(2024, 4, 18, 1, 0, 0),
     schedule="*/10 * * * *",
     catchup=True,
 ) as dag:
